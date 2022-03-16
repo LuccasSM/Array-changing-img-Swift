2f00d0272912c79a5f4329e421f590ef43431505
Luccas Santana Marinho <mathaluc2016@outlook.com>
2022-03-16 20:16:40 -0300

    firstLuccas

--- ArrayImages/ViewController.swift
+++ ArrayImages/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let header = UIView(frame: .zero)
+    
+    var imageArr = ["image1","image2"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        header.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(header)
+        header.backgroundColor  = .orange
+        header.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        header.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let RandomNumber = Int(arc4random_uniform(UInt32(self.imageArr.count)))
+        let image = UIImage.init(named: "\(imageArr[RandomNumber])")
+        let imageView = UIImageView.init(image: image)
+        imageView.frame = CGRect(x: imageView.frame.origin.x, y: 100, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2 - 50)
+        self.view.addSubview(imageView)
+        
+        imageArr.shuffle()
     }
-
-
 }
-
